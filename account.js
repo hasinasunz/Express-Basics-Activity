cca1bcd85b7fa8fc9cefd6d5ce5c4157e60595be
hasinasunz <sunzarh@gmail.com>
2021-02-08 09:58:43 -0500

    not complete

---
+++ account.js
@@ -0,0 +1,11 @@
+export default function balance () {
+function getBalance() {
+    console.log("get balance");
+}
+ function withdraw() {
+        console.log("withdrawal");
+    }
+ function deposit() {
+        console.log("deposit");
+    }
+}
--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 })
 
+app.get("/balance", (req, res)=> {
+    res.send(balance.toString());
+})
+
+app.get("/withdraw", (req, res) => {
+    const amountToWithdraw = req.body.amountToWithdraw;
+})
+
 app.listen(3000, () => {
     console.log("We're up and running");
 })
+
+// export default function balance () {
+//     function getBalance() {
+//         console.log("get balance");
+//     }
+//      function withdraw() {
+//             console.log("withdrawal");
+//         }
+//      function deposit() {
+//             console.log("deposit");
+//         }
+//     }
